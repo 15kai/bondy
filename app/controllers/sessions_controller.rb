cf0c2a55ba9b23e3242c74442fc6e151ef177dc6
pichico <hitopmifukushima@yahoo.co.jp>
2016-05-05 16:21:59 +0900

    ログイン後元いたアンケートに戻る

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_auth(request.env['omniauth.auth'])
     session[:user_id] = user.id
-    redirect_to root_path
+    return_back and return true
+    #redirect_to root_path
   end
 
   def destroy
